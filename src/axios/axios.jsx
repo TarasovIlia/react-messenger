e79f8e8fb58594e975807861c7a53ba5962b0480
Tarasov_Ilya <tarasov002@gmail.com>
2021-10-04 13:19:42 +0300

    add pagination

--- src/axios/axios.jsx
+++ src/axios/axios.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-//export const API_URL = 'https://back-test1.herokuapp.com'
+export const API_URL = 'https://back-test1.herokuapp.com'
 
 //for test
-export const API_URL = 'http://localhost:3000/'
+//export const API_URL = 'http://localhost:3000/'
 
 export const API = axios.create({
   baseURL: API_URL + '/',
