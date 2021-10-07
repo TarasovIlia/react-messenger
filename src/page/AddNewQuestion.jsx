727a7b9e5c5cb67dff27fbe85427a526e290b9ab
Tarasov_Ilya <tarasov002@gmail.com>
2021-10-07 13:30:54 +0300

    fix message on add new page

--- src/page/AddNewQuestion.jsx
+++ src/page/AddNewQuestion.jsx
@@ -5,7 +5,7 @@ import RadioTabs from'../components/RadioTabs.jsx';
 import { Link } from 'react-router-dom'
 
 export default function AddNewQuestion() {
-    const [sendConfirm, SetSendConfirm] = useState(false)
+    const [responce, setResponce] = useState(false)
     const [error, setError] = useState(false)
     const admin  = useSelector(state => state.user.value.currentUser.admin)
     const [formQuestion, setFormQuestion] = useState({
@@ -19,40 +19,33 @@ export default function AddNewQuestion() {
 
     const changeFormQuestion = event => {
         setFormQuestion({ ...formQuestion, [event.target.name] : event.target.value})
-        SetSendConfirm(false)
+        setResponce(false)
     } 
     const changeFormTopic = event => {
         setFormTopic({ ...formTopic, [event.target.name] : event.target.value})
-        SetSendConfirm(false)
+        setResponce(false)
     }
 
     const pushNewQuestion = async () => {
-        try {
-            if (formQuestion.question.length > 0 && formQuestion.topic.length > 0) {
-                await API.post('/api/question/send', {...formQuestion})
-                SetSendConfirm(true)
-                setFormQuestion({ question: ''})
-            } else {
-                setError(true)
-            }
-        } catch (error) {
-            console.log(error)
-        }
+        API.post('/api/question/send', {...formQuestion})
+                .then(responce => {
+                    setResponce(responce.data.message)
+                    setFormQuestion({ question: '', topic : selectedTopic})
+                })
+                .catch (err => {
+                    setError(err.response.data.message)
+                })
     }
 
     const pushNewTopic = async () => {
-        try {
-            if (formTopic.topic.length > 0) {
-                await API.post('/api/question/topic', {...formQuestion})
+        API.post('/api/question/topic', {...formTopic})
+            .then(responce => {
+                setResponce(responce.data.message)
                 setFormTopic({ topic : '', level : '1' })
-                SetSendConfirm(true)
-            }
-            else {
-                setError(true)
-            }
-        } catch (error) {
-            console.log(error)
-        }
+            })
+            .catch (err => {
+                setError(err.response.data.message)
+            })
     }
 
     if (!admin) {
@@ -100,15 +93,15 @@ export default function AddNewQuestion() {
                 className="button question-button"
                 >create new topic</button>
             </div>
-            {sendConfirm && 
+            {responce && 
             <div className='success-message'>
-                <p>Created successfully</p>
+                <p>{responce}</p>
             </div>
             }
             {error && 
             <div className='error-messsage'>
-                <p>pleace, enter all require data and select topic</p>
-                <button className='button-error' onClick={() => setError(!error)}>ok</button>
+                <p>{error}</p>
+                <button className='button-error' onClick={() => setError(false)}>ok</button>
             </div>
             }
         </div>
